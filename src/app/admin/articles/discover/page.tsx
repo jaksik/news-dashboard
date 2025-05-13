93c383f5c7ca9a60de27153d8f2d4743adec940e
jaksik <connorjaksik@gmail.com>
2025-05-13 09:28:49 -0500

    created new routes for dashboard

---
+++ src/app/admin/articles/discover/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function DiscoverArticlesPage() {
+  return (
+    <div className="p-4 md:p-6 2xl:p-10">
+      <div className="mb-6">
+        <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+          Discover Articles
+        </h2>
+      </div>
+      {/* Content will be added here later */}
+    </div>
+  );
+}
---
+++ src/app/admin/articles/manage/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function ManageArticlesPage() {
+  return (
+    <div className="p-4 md:p-6 2xl:p-10">
+      <div className="mb-6">
+        <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+          Manage Articles
+        </h2>
+      </div>
+      {/* Content will be added here later */}
+    </div>
+  );
+}
